3836d6bd7664f0322d073859de5a753da908e948
SakkeEU <60935845+SakkeEU@users.noreply.github.com>
2020-09-26 15:09:08 +0200

    update weakstomach.lua

--- scripts/components/weakstomach.lua
+++ scripts/components/weakstomach.lua
@@ -1,1 +1,13 @@
-
+local WeakStomach = Class(function(self, inst)
+	self.inst = inst
+	self.inst:AddTag("weakstomach")
+	self.ws = 2
+	self.inst:WatchWorldState("isday", function(inst) self.ws = 2 end)
+end)
+function WeakStomach:GetWS()
+	return self.ws
+end
+function WeakStomach:DecWS()
+	self.ws = self.ws - 1
+end
+return WeakStomach
